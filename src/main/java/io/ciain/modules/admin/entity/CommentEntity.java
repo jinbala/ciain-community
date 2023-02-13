ff43fefc399cb2e9a3c5ca98e374ebfb89548433
ciain <1623152676@qq.com>
2023-02-13 16:52:07 +0800

    修改注释

--- src/main/java/io/ciain/modules/admin/entity/CommentEntity.java
+++ src/main/java/io/ciain/modules/admin/entity/CommentEntity.java
@@ -1,15 +1,3 @@
-/**
- * -----------------------------------
- * 林风社交论坛开源版本请务必保留此注释头信息
- * 开源地址: https://gitee.com/virus010101/linfeng-community
- * 商业版演示站点: https://www.linfeng.tech
- * 商业版购买联系技术客服
- * QQ: 3582996245
- * 可正常分享和学习源码，不得专卖或非法牟利！
- * Copyright (c) 2021-2023 linfeng all rights reserved.
- * 版权所有 ，侵权必究！
- * -----------------------------------
- */
 package io.ciain.modules.admin.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -22,7 +10,7 @@ import java.util.Date;
 /**
  * 
  * 
- * @author linfeng
+ * @author chenjiajin
  * @email 3582996245@qq.com
  * @date 2022-01-24 21:29:22
  */
